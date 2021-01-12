7341f183290656a67b6ad0eb85c9fb370f3cd714
Gustavo Davis <gustavo.davis@outlook.com>
2021-01-12 18:22:13 -0300

    refactor para esquema do cenario

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -19,31 +19,6 @@ Então('sou redirecionado para o Dashboard') do
   sleep 5 #temporarily
 end
 
-Quando('submeto o meu cadastro sem o nome') do
-  find('#email').set Faker::Internet.free_email
-  find('#password').set "pwd123"
-  click_button  'Cadastrar'
-end
-
-Quando('submeto o meu cadastro sem o email') do
-  find('#fullName').set "Gustavo Davis"
-  find('#password').set "pwd123"
-  click_button  'Cadastrar'
-end
-
-Quando('submeto o meu cadastro com email incorreto') do
-  find('#fullName').set "Gustavo Davis"
-  find('#email').set "gustavo$gmail.com"
-  find('#password').set "pwd123"
-  click_button  'Cadastrar'
-end
-
-Quando('submeto o meu cadastro sem a senha') do
-  find('#fullName').set "Gustavo Davis"
-  find('#email').set Faker::Internet.free_email
-  click_button  'Cadastrar'
-end
-
 Então('vejo a mensagem de alerta: {string}') do |expect_alert|
   alert = find('.alert-dark')
   expect(alert.text).to eql expect_alert
