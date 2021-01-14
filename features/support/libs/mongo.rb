9d004a4842787449dd1d0bff5ab4bb5a43d67d22
Gustavo Davis <gustavo.davis@outlook.com>
2021-01-14 18:28:08 -0300

    Refactor da classe MongoDB

--- features/support/libs/mongo.rb
+++ features/support/libs/mongo.rb
@@ -3,23 +3,25 @@ require 'mongo'
 Mongo::Logger.logger = Logger.new('./logs/mongo.log')
 
 class MongoDB
-  def remove_user(email)
+  attr_accessor :users, :equipos
+
+  def initialize
     client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-    users = client[:users]
+    @users = client[:users]
+    @equipos = client[:equipos]
+  end
+  
+  def remove_user(email)    
     users.delete_many({email: email})
   end
 
   def get_user(email)
-    client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-    users = client[:users]
     user = users.find({email: email}).first
     return user[:_id]
   end
 
   def remove_equip(name, email)
-    user_id = get_user(email)
-    client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-    equipos = client[:equipos]
+    user_id = get_user(email)    
     equipos.delete_many({name: name, user: user_id})
   end
 end
