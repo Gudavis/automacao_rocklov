9263d470cc0adb20f83fd87fec9932ac7c8be5ee
Gustavo Davis <gustavo.davis@outlook.com>
2021-03-15 16:13:26 -0300

    Add env config and hook to get screenshot

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,9 +3,24 @@ require "capybara/cucumber"
 require "allure-cucumber"
 require "faker"
 
+CONFIG = YAML::load_file(File.join(__dir__, "config/#{ENV["ENVIRONMENT"]}"))
+
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "fire_headless"
+  @driver = :selenium_headless
+when "chrome"
+  @driver = :selenium_chrome
+when "chrome_headless"
+  @driver = :selenium_chrome_headless
+else
+  raise "Navegador inválido"
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
-  config.app_host = "http://rocklov-web:3000"
+  config.default_driver = @driver
+  config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
 
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,3 +7,13 @@ Before do
 
   page.current_window.resize_to(1440, 900)
 end
+
+After do
+  temp_screenshot = page.save_screenshot("logs/temp_screenshot.png")
+
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_screenshot),
+  )
+end
